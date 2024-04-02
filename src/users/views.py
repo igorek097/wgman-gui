09d17f162ee1b93068e24505216c6560b7e0aaf4
igorek097 <igorek097@gmail.com>
2024-04-02 14:34:03 +0200

    1

--- src/users/views.py
+++ src/users/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
 from django.contrib.auth import views as auth_views, get_user_model
+from django.contrib.auth import logout
 
 from users import forms
 
@@ -11,6 +13,11 @@ class SignupView(CreateView):
     form_class = forms.SignupForm
     model = get_user_model()
     template_name = 'users/signup.html'
+    
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        if get_user_model().objects.count():
+            return HttpResponseRedirect(reverse('core:main'))
+        return super().get(request, *args, **kwargs)
 
     
 #TODO: figure out redirect logic and optimize
@@ -23,6 +30,12 @@ class LoginView(auth_views.LoginView):
     fields = ['username', 'password']
     template_name = 'users/login.html'
     
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('core:main'))
+        return super().get(request, *args, **kwargs)
+
+    
     def get_success_url(self) -> str:
         return self.success_url
     
@@ -31,3 +44,7 @@ class LogoutView(auth_views.LogoutView):
     
     http_method_names = ['get']
     template_name = 'users/logout.html'
+    
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        logout(request)
+        return super().get(request, *args, **kwargs)
