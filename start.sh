1d9e45d4051f89db5e2830988932958fb2f3c9db
igorek097 <igorek097@gmail.com>
2024-04-11 17:00:55 +0200

    1

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python /app/src/manage.py initwgm && python gunicorn --chdir /app/src app.wsgi --bind 127.0.0.1:8888
+python /app/src/manage.py initwgm && gunicorn --chdir /app/src app.wsgi --bind 127.0.0.1:8888
